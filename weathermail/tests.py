17e749958dbb8d82b206c59872f8365dec5e2fc1
Ryan G. Hunter <ryan@cens.io>
2018-03-04 16:52:26 -0500

    add some unit test coverage to the models

--- weathermail/tests.py
+++ weathermail/tests.py
@@ -2,6 +2,10 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from django.db import IntegrityError
+from django.core.exceptions import ValidationError
+
+
 
 from .models import Location, Subscriber
 
@@ -13,3 +17,32 @@ class LocationTestCase(TestCase):
         """
         l = Location(city="Boston", state="MA")
         self.assertEqual(str(l), "Boston, MA")
+
+    def test_unique(self):
+        """
+        Test that we get an error if we try to add the same city twice. We expect an Integrity Error
+        """
+        Location(city="Boston", state="MA").save()
+        with self.assertRaises(IntegrityError):
+            Location(city="Boston", state="MA").save()
+
+
+class SubsriberTestCase(TestCase):
+
+    def setUp(self):
+        self.location = Location(city="Boston", state="MA")
+        self.location.save()
+
+    def test_email_validation(self):
+        with self.assertRaises(ValidationError):
+            Subscriber(location=self.location, email_address="ryan.g.hunter").clean_fields()
+
+
+    def test_unique_email(self):
+        """
+        Test that we get an integrity error if we try and save the same email twice
+        """
+        Subscriber(location=self.location, email_address="ryan.g.hunter@gmail.com").save()
+        with self.assertRaises(IntegrityError):
+            Subscriber(location=self.location, email_address="ryan.g.hunter@gmail.com").save()
+
