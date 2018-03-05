e42b279ad53354bfabd30d34ce8c36845d66d3bf
Ryan G. Hunter <ryan@cens.io>
2018-03-04 20:35:27 -0500

    added some saftey around load locations. Warn the user if the operation will wipe out existing subscriber records

--- weathermail/management/commands/loadlocations.py
+++ weathermail/management/commands/loadlocations.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
-from weathermail.models import Location
+from weathermail.models import Location, Subscriber
+import sys
 
 import requests
 
@@ -9,7 +10,19 @@ class Command(BaseCommand):
     QUERY = "https://public.opendatasoft.com/api/records/1.0/search/?dataset=1000-largest-us-cities-by-population-with-geographic-coordinates&rows=100&sort=-rank"
 
     def handle(self, *args, **options):
+        ## Running this command will delete all existing subscriber records.
+        if len(Subscriber.objects.all()) > 0:
+            print "Existing subscriber records detected. If you continue running this command, all subscriber records will be deleted."
+
+            resp = raw_input("Are you sure you want to continue? [yes/no]: ")
+            if resp != "yes":
+                print "Exiting without touching records"
+                sys.exit()
+        else:
+            print "No existing subscriber records found."
         ## First, lets truncate any existing records
+        print "Clearing the database"
+        Subscriber.objects.all().delete()
         Location.objects.all().delete()
 
         ## Load cities from opendatasoft
