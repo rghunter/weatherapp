f5398be989da4d3d3c84fbc601bac9e4ac627440
Ryan G. Hunter <ryan@cens.io>
2018-03-17 21:42:46 -0400

    wunderground api

--- weathermail/models/wunderground.py
+++ weathermail/models/wunderground.py
@@ -1,49 +1,80 @@
 import requests
 
+class WundergroundAPIException(Exception):
+    pass
 
-class InvalidLocation(Exception):
+class InvalidLocationException(WundergroundAPIException):
     """
     raised when a given location fails validation
     """
     pass
 
-class WundergrounAPI(object):
+class WundergroundAPI(object):
 
     def __init__(self, api_key):
         self.api_key = api_key
 
 
-    def __api_call(self, data_feature, city, state):
-        if len(state) > 2:  # Not a very smart validator, but this will catch scenarios where state name instead of state apprv is used
-            raise InvalidLocation("Invalid State: {}. Make sure your useing the shortcode (ie. MA for Massachuesetts)".format(state))
-        return requests.get("http://api.wunderground.com/api/{api_key}/{data_feature}/q/{state}/{city}.json".format(
-                api_key=self.api_key, data_feature=data_feature, city=city, state=state)).json()
+    @staticmethod
+    def join_features(data_features):
+        """
+        take a list of datafeatures and return a list the API will accept
+        i.e. ['a', 'b'] becomes a/b
+        """
+        if not isinstance(data_features, list):
+            raise WundergroundAPIException("data_features must be a list of features")
+        return "/".join(data_features)
 
-    def conditions(self, city, state):
-        """ Wunderground API conditions
-        for a given city and state, return the current weather conditions
+    def api_call_datafeature(self, data_features, city, state):
+        resp = requests.get("http://api.wunderground.com/api/{api_key}/{data_feature_string}/q/{state}/{city}.json".format(
+            api_key=self.api_key, data_feature_string=self.join_features(data_features), city=city, state=state))
+
+        # Check that we get a 200 status code
+        if not resp.status_code == 200:
+            raise WundergroundAPIException("API returned: {} Mesasge: {}".format(resp.status_code, resp.text))
+        parsed_response = resp.json()['response']
+
+        # Wunderground API validation always returns a 200 status code, but will include an errors key if errors are present in the request
+        if "error" in parsed_response.keys():
+            raise WundergroundAPIException("API returned an error: {}".format(parsed_response['error']['type']))
+
+        # Check if the location is valid. If geolookup fails, we will get a results key in the response object
+        if "results" in parsed_response.keys():
+            raise InvalidLocationException("The location you provided failed geolookup: city: {} state: {}".format(city, state))
+
+        return resp.json()
+
+    def __avg_temp(self, almanac):
+        def temp(t):
+            return t['normal']['F']
+        high = float(temp(almanac['temp_high']))
+        low = float(temp(almanac['temp_low']))
+        return (high+low)/2.0
+
+    def fetch_data(self, city, state):
+        """ Wunderground API conditions and Almanac data
+        for a given city and state, return the current weather conditions and historical temps
         API Docs: https://www.wunderground.com/weather/api/d/docs?d=data/conditions
+        API Docs: https://www.wunderground.com/weather/api/d/docs?d=data/almanac
+
         :param city: string (the city)
         :param state: string (the state shortcode i.e MA for massachusetts)
         :return current temp, average temp, conditions, condition icon
         {
             "temperature": <temp in farenhieght>
-            "conditions": <condition string>
+            "condition": <condition string>
             "condition_icon": <condition icon>
+            "average_temp": <float mean temperature> 
         }
         """
-        pass
+        resp = self.api_call_datafeature(["almanac","conditions"], city, state)
+        conditions = resp['current_observation']
+
+        return dict(
+                temperature=conditions['temp_f'],
+                condition=str(conditions['weather']),
+                condition_icon=str(conditions['icon_url']),
+                average_temp=self.__avg_temp(resp["almanac"])
+                )
 
 
-    def historical(self, city, state):
-        """
-        Get the historical average temperature for a given location
-
-        I generate an average temperature by taking the mean of the average high and low
-
-        :param city: string (the city)
-        :param state: string (the state shortcode i.e MA for massachusetts)
-        :return { "average_temp": <float mean temperature> }
-        """
-        resp = self.__api_call(data_feature="almanac", city=city, state=state)
-        pass
--- weathermail/tests/test_wunderground.py
+++ weathermail/tests/test_wunderground.py
@@ -1,51 +1,40 @@
 from django.test import TestCase
 
 import requests_mock
+import mock, json
 
-from weathermail.models.wunderground import WundergrounAPI
+from weathermail.models.wunderground import WundergroundAPI, WundergroundAPIException, InvalidLocationException
 
 class WundergroundAPITestCase(TestCase):
 
-    almanac_query = "http://api.wunderground.com/api/API_KEY/almanac/q/ma/boston.json"
-    almanac_response = """
-        {
-            "response": {
-                "version": "0.1",
-                "termsofService": "http://www.wunderground.com/weather/api/d/terms.html",
-                "features": {
-                    "almanac": 1
-                }
-            },
-            "almanac": {
-                "airport_code": "KBOS",
-                "temp_high": {
-                    "normal": {
-                        "F": "46",
-                        "C": "8"
-                    },
-                    "record": {
-                        "F": "70",
-                        "C": "21"
-                    },
-                    "recordyear": "1990"
-                },
-                "temp_low": {
-                    "normal": {
-                        "F": "31",
-                        "C": "-1"
-                    },
-                    "record": {
-                        "F": "10",
-                        "C": "-12"
-                    },
-                    "recordyear": "1916"
-                }
-            }
-        }
-        """
+    error_query = "http://api.wunderground.com/api/API_KEY/alsdfmanac/q/ma/boston.json"
+    error_response= """
+    {
+       "response": {
+           "version": "0.1",
+           "termsofService": "http://www.wunderground.com/weather/api/d/terms.html",
+           "features": {},
+           "error": {
+               "type": "unknownfeature"
+           }
+       }
+    }
+    """
+
+    bad_loc_query = "http://api.wunderground.com/api/API_KEY/almanac/q/ca/boston.json"
+    bad_loc_response= """
+    {
+       "response": {
+           "version": "0.1",
+           "termsofService": "http://www.wunderground.com/weather/api/d/terms.html",
+           "features": {},
+           "results": []
+       }
+    }
+    """
 
 
-    conditions_query = "http://api.wunderground.com/api/API_KEY/conditions/q/ma/boston.json"
+    fetch_query = "http://api.wunderground.com/api/API_KEY/almanac/conditions/q/ma/boston.json"
     conditions_response = """
         {
            "response": {
@@ -142,16 +131,154 @@ class WundergroundAPITestCase(TestCase):
         }
         """
 
+    fetch_data_response = json.loads("""
+        {
+            "response": {
+                "version": "0.1",
+                "termsofService": "http://www.wunderground.com/weather/api/d/terms.html",
+                "features": {
+                    "almanac": 1,
+                    "conditions": 1
+                }
+            },
+            "current_observation": {
+                "image": {
+                    "url": "http://icons.wxug.com/graphics/wu2/logo_130x80.png",
+                    "title": "Weather Underground",
+                    "link": "http://www.wunderground.com"
+                },
+                "display_location": {
+                    "full": "Boston, MA",
+                    "city": "Boston",
+                    "state": "MA",
+                    "state_name": "Massachusetts",
+                    "country": "US",
+                    "country_iso3166": "US",
+                    "zip": "02108",
+                    "magic": "1",
+                    "wmo": "99999",
+                    "latitude": "42.36000061",
+                    "longitude": "-71.06999969",
+                    "elevation": "21.9"
+                },
+                "observation_location": {
+                    "full": "North End, Boston, Boston, Massachusetts",
+                    "city": "North End, Boston, Boston",
+                    "state": "Massachusetts",
+                    "country": "US",
+                    "country_iso3166": "US",
+                    "latitude": "42.366951",
+                    "longitude": "-71.056152",
+                    "elevation": "69 ft"
+                },
+                "estimated": {},
+                "station_id": "KMABOSTO132",
+                "observation_time": "Last Updated on March 17, 9:19 PM EDT",
+                "observation_time_rfc822": "Sat, 17 Mar 2018 21:19:34 -0400",
+                "observation_epoch": "1521335974",
+                "local_time_rfc822": "Sat, 17 Mar 2018 21:19:43 -0400",
+                "local_epoch": "1521335983",
+                "local_tz_short": "EDT",
+                "local_tz_long": "America/New_York",
+                "local_tz_offset": "-0400",
+                "weather": "Clear",
+                "temperature_string": "25.2 F (-3.8 C)",
+                "temp_f": 25.2,
+                "temp_c": -3.8,
+                "relative_humidity": "24%",
+                "wind_string": "From the NW at 18.6 MPH Gusting to 24.8 MPH",
+                "wind_dir": "NW",
+                "wind_degrees": 313,
+                "wind_mph": 18.6,
+                "wind_gust_mph": "24.8",
+                "wind_kph": 29.9,
+                "wind_gust_kph": "39.9",
+                "pressure_mb": "1009",
+                "pressure_in": "29.79",
+                "pressure_trend": "+",
+                "dewpoint_string": "-6 F (-21 C)",
+                "dewpoint_f": -6,
+                "dewpoint_c": -21,
+                "heat_index_string": "NA",
+                "heat_index_f": "NA",
+                "heat_index_c": "NA",
+                "windchill_string": "12 F (-11 C)",
+                "windchill_f": "12",
+                "windchill_c": "-11",
+                "feelslike_string": "12 F (-11 C)",
+                "feelslike_f": "12",
+                "feelslike_c": "-11",
+                "visibility_mi": "10.0",
+                "visibility_km": "16.1",
+                "solarradiation": "0",
+                "UV": "0.0",
+                "precip_1hr_string": "0.00 in ( 0 mm)",
+                "precip_1hr_in": "0.00",
+                "precip_1hr_metric": " 0",
+                "precip_today_string": "0.00 in (0 mm)",
+                "precip_today_in": "0.00",
+                "precip_today_metric": "0",
+                "icon": "clear",
+                "icon_url": "http://icons.wxug.com/i/c/k/nt_clear.gif",
+                "forecast_url": "http://www.wunderground.com/US/MA/Boston.html",
+                "history_url": "http://www.wunderground.com/weatherstation/WXDailyHistory.asp?ID=KMABOSTO132",
+                "ob_url": "http://www.wunderground.com/cgi-bin/findweather/getForecast?query=42.366951,-71.056152",
+                "nowcast": ""
+            },
+            "almanac": {
+                "airport_code": "KBOS",
+                "temp_high": {
+                    "normal": {
+                        "F": "46",
+                        "C": "8"
+                    },
+                    "record": {
+                        "F": "70",
+                        "C": "21"
+                    },
+                    "recordyear": "1990"
+                },
+                "temp_low": {
+                    "normal": {
+                        "F": "31",
+                        "C": "-1"
+                    },
+                    "record": {
+                        "F": "10",
+                        "C": "-12"
+                    },
+                    "recordyear": "1916"
+                }
+            }
+        }
+        """)
+
     def setUp(self):
-        self.api_client = WundergrounAPI("API_KEY")  ## instantiate the client with a fake api key
+        self.api_client = WundergroundAPI("API_KEY")  ## instantiate the client with a fake api key
+
 
     @requests_mock.mock()
-    def test_almanac(self, m):
-        m.get(self.almanac_query, text=self.almanac_response)
-        resp = self.api_client.historical("boston", "ma")
-        self.assertEqual(resp, dict(average_temp=28))
+    def test_api_error(self, m):
+        m.get(self.error_query, text=self.error_response)
+        self.assertRaises(WundergroundAPIException, self.api_client.api_call_datafeature, ["alsdfmanac"], "boston", "ma")
+
 
     @requests_mock.mock()
-    def test_conditions(self, m):
-        m.get(self.conditions_query, text=self.conditions_response)
-        resp = self.api_client.conditions("boston", "ma")
+    def test_api_status_code_error(self, m):
+        m.get(self.error_query, status_code=500) # Since we are testing for an actual status code return, the query URL is more or less irrelevant for this test.
+        self.assertRaises(WundergroundAPIException, self.api_client.api_call_datafeature, ["alsdfmanac"], "boston", "ma")
+
+    @requests_mock.mock()
+    def test_bad_loc(self, m):
+        m.get(self.bad_loc_query, text=self.bad_loc_response)
+        self.assertRaises(InvalidLocationException, self.api_client.api_call_datafeature, ["almanac"], "boston", "ca")
+
+    def test_join_features(self):
+        resp = self.api_client.join_features(['a', 'b'])
+        self.assertEqual('a/b', resp)
+
+    @mock.patch.object(WundergroundAPI, 'api_call_datafeature') 
+    def test_fetch_data(self, m):
+        m.return_value = self.fetch_data_response
+        resp = self.api_client.fetch_data("boston", "ma")
+        self.assertEqual(resp, dict(temperature=25.2, condition="Clear", condition_icon="http://icons.wxug.com/i/c/k/nt_clear.gif", average_temp=38.5))
